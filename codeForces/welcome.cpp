6cda79d27bfdce573101cda65da8d198945aa9f4
RafyHany <rafyhany1@gmail.com>
2024-03-14 16:23:00 +0200

    problems

--- codeForces/welcome.cpp
+++ codeForces/welcome.cpp
@@ -1,12 +1,48 @@
-#include<iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
-void add(){
-    cout << "hhhhh" <<endl ;
-}
-int main (){
 
-    cout << "hello world" ;
-    add() ;
-    return 0 ;
+int main()
+{
+//    int t = 0;
+//    cin >> t ;
+//    while (t--)
+//    {
+//        int l , a , b ;
+//        cin >> a >> b >> l ;
+//        int k = 1 ;
+//        bool found = false ;
+//        int res = 0 ;
+//        while(k <= l){
+//            if( l % k != 0){
+//                k++;
+//                continue ;
+//            }
+//
+//            for(int i = 0 ; !found ; ++i ){
+//                if(l < k * pow(a , i) ){
+//                        break ;
+//                    }
+//                for(int j = 0 ; !found ; ++j){
+//                    if(l == k * pow(a , i) * pow(b , j)){
+//                        res++;
+//                        found = true ;
+//                        break;
+//                    }
+//                    if(l < k * pow(a , i) * pow(b , j)){
+//                        break ;
+//                    }
+//
+//                }
+//            }
+//            found = false ;
+//            k++;
+//
+//        }
+//        cout << res << endl ;
+//
+//    }
+cout << "hello " ;
+    return 0;
 }
---
+++ leetCode/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("rafy thanks here!\n");
+    return 0;
+}
+
+
+int rr(){
+    return 0 ;
+}
