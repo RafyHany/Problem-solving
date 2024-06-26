1a97b3e56d67c9b64c16cd63616f8b27a48437d9
Rafi Hany Said Ansy Abdel Malak <121082388+RafyHany@users.noreply.github.com>
2024-06-26 17:35:17 +0300

    Time: 149 ms (5.27%), Space: 46.9 MB (5.2%) - LeetHub

--- 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.java
+++ 1285-balance-a-binary-search-tree/1285-balance-a-binary-search-tree.java
@@ -14,8 +14,7 @@
  * }
  */
 class Solution {
-    TreeNode newRoot ;
-
+    TreeNode newRoot ; // new node to be returned 
 
     public TreeNode balanceBST(TreeNode root) {
         ArrayList<Integer> set = new ArrayList<>() ;
@@ -23,29 +22,32 @@ class Solution {
         divideAndConquer(0 , set.size()-1 , set );
         return newRoot ;
     }
-    private void inOrderTraverse(TreeNode node , ArrayList<Integer> traverse){
+    private void inOrderTraverse(TreeNode node , ArrayList<Integer> traverse){ // apply in-order travers to sort the nodes
         if(node == null)
             return;
         inOrderTraverse(node.left , traverse);
         traverse.add(node.val);
         inOrderTraverse(node.right,traverse);
     }
-    private TreeNode addTotree(TreeNode root , int newNode){
+    private TreeNode addTotree(TreeNode root , int newNode){ // add node to bst
         if(root == null){
-            return  new TreeNode(newNode) ;        }
+            System.out.println("hi " + newNode);
+            return newRoot = new TreeNode(newNode) ;
+        }
         if(root.val > newNode)
             root.left = addTotree(root.left , newNode) ;
         else if(root.val < newNode)
             root.right = addTotree(root.right , newNode) ;
         return root ;
     }
-    private void divideAndConquer(int l , int r ,ArrayList<Integer> set ){
+    private void divideAndConquer(int l , int r ,ArrayList<Integer> set ){ // add the middle of array to bst to achieve it balance 
         if(l > r)
             return;
         int mid = (l+r)/2 ;
-        newRoot = addTotree(newRoot , set.get(mid));
+        newRoot = addTotree(newRoot , set.get(mid)); 
         divideAndConquer(l , mid-1, set );
         divideAndConquer(mid + 1 , r, set );
 
     }
+
 }
