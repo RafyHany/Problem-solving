cad793a7d9e56b6bb9023bdc4e00c81c97fd34a4
Rafi Hany Said Ansy Abdel Malak <121082388+RafyHany@users.noreply.github.com>
2024-07-02 17:37:49 +0300

    Time: 3 ms (90.95%), Space: 42.7 MB (92.32%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,27 +1,48 @@
 class Solution {
-    public int[] intersect(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> freq = new HashMap<>();
+    public int[] intersect(int[] nums1, int[] nums2){
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int i = 0 , j = 0 ;
         ArrayList<Integer> res = new ArrayList<>();
-        for(int ele : nums1){
-            if(freq.containsKey(ele))
-                freq.put(ele,freq.get(ele) + 1);
-            else
-                freq.put(ele,1) ;
-        }
-        for(int ele : nums2){
-            if(freq.containsKey(ele)){
-                res.add(ele) ;
-                int freqs = freq.get(ele) ;
-                if(freqs == 1)
-                    freq.remove(ele);
-                else
-                    freq.put(ele , freqs - 1) ;
-            }
-
+        while (i < nums1.length && j < nums2.length){
+            if(nums1[i] == nums2[j]){
+                res.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) 
+                i++ ;
+            else 
+                j++ ;
         }
         int[] finalRes = new int[res.size()] ;
-        for(int i = 0 ; i < res.size() ; i++ )
-            finalRes[i] = res.get(i) ;
-        return  finalRes;
+        for(int k = 0 ; k < res.size() ; k++ )
+            finalRes[k] = res.get(k) ;
+        return finalRes ;
     }
+
+    //    public int[] intersect(int[] nums1, int[] nums2) {
+//        HashMap<Integer,Integer> freq = new HashMap<>();
+//        ArrayList<Integer> res = new ArrayList<>();
+//        for(int ele : nums1){
+//            if(freq.containsKey(ele))
+//                freq.put(ele,freq.get(ele) + 1);
+//            else
+//                freq.put(ele,1) ;
+//        }
+//        for(int ele : nums2){
+//            if(freq.containsKey(ele)){
+//                res.add(ele) ;
+//                int freqs = freq.get(ele) ;
+//                if(freqs == 1)
+//                    freq.remove(ele);
+//                else
+//                    freq.put(ele , freqs - 1) ;
+//            }
+//
+//        }
+//        int[] finalRes = new int[res.size()] ;
+//        for(int i = 0 ; i < res.size() ; i++ )
+//            finalRes[i] = res.get(i) ;
+//        return  finalRes;
+//    }
 }
