a64aca2a014b3d89f747de083a1a52f397c120a1
Rafi Hany Said Ansy Abdel Malak <121082388+RafyHany@users.noreply.github.com>
2024-07-11 19:55:34 +0300

    Time: 0 ms (100%), Space: 42 MB (50.17%) - LeetHub

--- 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.java
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.java
@@ -1,5 +1,48 @@
 class Solution {
+    int index = 0 ;
+
     public String reverseParentheses(String s) {
+        return reverseParentheses2(s);
+    }
+    public String reverseParentheses2(String s ) {
+        StringBuilder str = new StringBuilder() ;
+        while(index < s.length()){
+            if(s.charAt(index) == ')'){
+                index++ ;
+                return str.reverse().toString();
+            }
+            if(s.charAt(index) == '('){
+                index ++ ;
+                String temp = reverseParentheses2(s) ;
+                str.append(temp) ;
+            }else {
+                str.append(s.charAt(index)) ;
+                index++ ;
+            }
+        }
+        return str.toString() ;
+    }    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*public String reverseParentheses(String s) {
         StringBuilder str ;
         Stack<Character> stack = new Stack<>();
         for(int i = 0 ; i < s.length() ; i++){
@@ -19,5 +62,5 @@ class Solution {
         while(!stack.isEmpty())
             str.append(stack.pop());
         return str.reverse().toString() ;
-    }
+    }*/
 }
