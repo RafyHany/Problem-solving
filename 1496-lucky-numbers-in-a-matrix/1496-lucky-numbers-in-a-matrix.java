eef0652f2672355873c3ec90590ac834e91adfb4
Rafi Hany Said Ansy Abdel Malak <121082388+RafyHany@users.noreply.github.com>
2024-07-19 09:39:58 +0300

    Time: 6 ms (9.24%), Space: 45.1 MB (51.1%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.java
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.java
@@ -31,3 +31,44 @@ class Solution {
         return res ;
     }
 }
+
+/*
+
+public List<Integer> luckyNumbers (int[][] matrix) {
+        List<Integer> lsRes = new ArrayList<>();
+
+        for(int i = 0; i < matrix.length; i++)
+        {
+            int col = findMin(matrix, i);
+
+            int maxCol = matrix[i][col];
+            if(maxInCol(matrix, maxCol, col))
+                lsRes.add(maxCol);
+        }
+
+        return lsRes;
+    }
+    private int findMin(int[][] matrix, int row){
+		int val= matrix[row][0], col=0;
+		for(int i=1;i<matrix[row].length;i++){
+			if(matrix[row][i]<val){
+				val=matrix[row][i];
+				col=i;
+			}
+		}
+		return col;
+	}
+    private boolean maxInCol(int[][] matrix, int val, int col){
+        for(int i=0;i<matrix.length;i++){
+            if(matrix[i][col]>val)
+                return false;
+        }
+        return true;
+    }
+
+
+
+
+
+
+*/ 
