064d6b89348dfb2b4cc2cced388c360f9be4b9e7
Rafi Hany Said Ansy Abdel Malak <121082388+RafyHany@users.noreply.github.com>
2024-07-22 17:18:02 +0300

    Time: 7 ms (90.64%), Space: 45.5 MB (18.45%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.java
+++ 2502-sort-the-people/2502-sort-the-people.java
@@ -6,20 +6,11 @@ class Solution {
         for(int i = 0 ; i < size ; ++i)
             map.put(heights[i] , names[i]) ;
         Arrays.sort(heights);
-        reverse(heights);
+        int index = 0 ;
         String[] result = new String[size] ;
-        for(int i = 0 ; i < size ; ++i)
-            result[i] = map.get(heights[i]) ;
-        for(int i = 0 ; i < size ; ++i)
-            System.out.print(heights[i] + " ");
+        for(int i = size - 1 ; i >= 0 ; i--)
+            result[index++] = map.get(heights[i]) ;
+        
         return result ;
-    }
-    private void reverse (int[] arr){
-        int size = arr.length ;
-        for(int i = 0 ; i < size/2 ; ++i){
-            int temp = arr[i] ;
-            arr[i] = arr[size - i - 1] ;
-            arr[size - i - 1] = temp ;
-        }
     }
 }
