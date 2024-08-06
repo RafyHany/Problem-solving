4f2011694478b30d28b80e8dca1911111b9fd5bd
Rafi Hany Said Ansy Abdel Malak <121082388+RafyHany@users.noreply.github.com>
2024-08-06 17:24:44 +0300

    Time: 8 ms (91.92%), Space: 46.1 MB (9.23%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.java
@@ -3,24 +3,16 @@ class Solution {
         int[] freq = new int[26] ;
         for(int i = 0 ; i < word.length() ; ++i)
             freq[(int)word.charAt(i) - 97] ++ ;
-        List<MapCharacterWithFreq> sortedFreq = new ArrayList<>();
-        for(int i = 0 ; i < freq.length ;++i)
-            sortedFreq.add(new MapCharacterWithFreq((char)(i+97) , freq[i])) ;
-        sortedFreq.sort((a, b) -> b.freq - a.freq);
+        
+        Arrays.sort(freq);
         int numberOfCharacterPerKey = 0 ;
         int res = 0 ;
-        for(MapCharacterWithFreq key : sortedFreq){
-            res += ((numberOfCharacterPerKey++/8)+1) * key.freq;
+        for(int i = freq.length - 1; i >= 0 ; i--){
+            if(freq[i] == 0)
+                break;
+            res += ((numberOfCharacterPerKey++/8)+1) * freq[i];
         }
         return  res ;
     }
-    class MapCharacterWithFreq {
-        char c ;
-        int freq ;
-        MapCharacterWithFreq(char c , int freq){
-            this.c = c ;
-            this.freq = freq ;
-        }
-
-    }
+    
 }
