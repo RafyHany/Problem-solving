ffce4cf2392fd1f77245f4e89403ce2eb28830c5
Rafi Hany Said Ansy Abdel Malak <121082388+RafyHany@users.noreply.github.com>
2024-08-14 14:21:17 +0300

    Time: 2 ms (99.72%), Space: 43.2 MB (37.75%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,23 +1,25 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List < List < Integer >> ans = new ArrayList < > ();
+        List<List<Integer>> res = new ArrayList<>() ;
         Arrays.sort(candidates);
-        findCombinations(0, candidates, target, ans, new ArrayList < > ());
-        return ans;
+        findCombination(0 , target , candidates , res , new ArrayList<>());
+        return res ;
     }
-    static void findCombinations(int ind, int[] arr, int target, List < List < Integer >> ans, List < Integer > ds) {
-        if (target == 0) {
-            ans.add(new ArrayList < > (ds));
+
+    private void findCombination(int index , int target , int[] candidates , List<List<Integer>> res , List<Integer> tempRes ){
+        if(target == 0 ){
+            res.add(new ArrayList<>(tempRes)) ;
             return;
         }
-
-        for (int i = ind; i < arr.length; i++) {
-            if (i > ind && arr[i] == arr[i - 1]) continue;
-            if (arr[i] > target) break;
-
-            ds.add(arr[i]);
-            findCombinations(i + 1, arr, target - arr[i], ans, ds);
-            ds.remove(ds.size() - 1);
+        for(int i = index ; i < candidates.length ; ++i){
+            if(i > index && candidates[i]==candidates[i-1]) 
+                continue;
+            if(candidates[i] > target)
+                break;
+            tempRes.add(candidates[i]) ;
+            findCombination(i + 1 , target - candidates[i]  , candidates, res , tempRes);
+            tempRes.removeLast() ;
         }
+            
     }
 }
