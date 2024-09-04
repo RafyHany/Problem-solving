638f70722ac0c359b90d9c490c01660ba7a7a748
Rafi Hany Said Ansy Abdel Malak <121082388+RafyHany@users.noreply.github.com>
2024-09-04 13:46:16 +0300

    Time: 37 ms (34.06%), Space: 49.4 MB (57.64%) - LeetHub

--- 0906-walking-robot-simulation/0906-walking-robot-simulation.java
+++ 0906-walking-robot-simulation/0906-walking-robot-simulation.java
@@ -2,32 +2,63 @@ import java.util.HashSet;
 import java.util.Set;
 
 class Solution {
-    public int robotSim(int[] commands, int[][] obstacles) {
-        int x=0,y=0,d=0;
-        int[][] direction={{0,1},{1,0},{0,-1},{-1,0}};
-        int maxDistance=0;
-        Set<String> obstacleSet=new HashSet<>();
-        for(int[] obstacle:obstacles){
-            obstacleSet.add(obstacle[0]+","+obstacle[1]);
+    int direction = 0 ;
+    int right = 0 ;
+    int top = 1 ;
+    private void setDirections(int directions){
+        if(directions == -1)
+            direction = (direction + 1) % 4 ;
+        else
+            direction = (direction + 3) % 4 ;
+        switch(direction){
+            case 0 :
+                right = 0 ;
+                top = 1 ;
+                break;
+            case 1 :
+                right = 1 ;
+                top = 0 ;
+                break;
+            case 2 :
+                right = 0 ;
+                top = -1 ;
+                break;
+            case 3 :
+                right = -1 ;
+                top = 0 ;
+                break;
         }
-        for(int cmd:commands){
-            if(cmd==-1){
-                d=(d+1)%4;
-            }else if(cmd==-2){
-                d=(d+3)%4;
-            }else{
-                for(int i=0;i<cmd;i++){
-                    int nx=x+direction[d][0];
-                    int ny=y+direction[d][1];
-                    if(obstacleSet.contains(nx+","+ny)){
-                        break;
+    }
+    private int euclideanDistance(int x , int y){
+        return x * x + y * y ;
+    }
+
+        public int robotSim(int[] commands, int[][] obstacles) {
+            int x = 0 , y = 0 ;
+            int maxDistance=0;
+            Set<String> obstacleSet=new HashSet<>();
+            for(int[] obstacle:obstacles){
+                obstacleSet.add(obstacle[0]+","+obstacle[1]);
+            }
+            for(int cmd:commands){
+                if(cmd < 0){
+                    setDirections(cmd) ;
+                }else{
+                    for(int i=0;i<cmd;i++){
+                        int nx = x + right;
+                        int ny = y + top;
+                        if(obstacleSet.contains(nx+","+ny)){
+                            break;
+                        }
+                        x = nx;
+                        y = ny;
+                        maxDistance=Math.max(maxDistance,euclideanDistance(x , y));
                     }
-                    x=nx;
-                    y=ny;
-                    maxDistance=Math.max(maxDistance,x*x+y*y);
                 }
             }
+            return maxDistance;
         }
-        return maxDistance;
-    }
+
 }
+
+
