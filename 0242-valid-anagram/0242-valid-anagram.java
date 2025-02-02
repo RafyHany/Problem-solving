5ee18b18685bc03c2d56eb77d1c126473f4fe801
Rafi Hany Said Ansy Abdel Malak <121082388+RafyHany@users.noreply.github.com>
2025-02-02 13:10:38 +0200

    Time: 15 ms (27.48%), Space: 44.8 MB (26.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,9 +1,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] arrS = s.toCharArray() ;
-        char[] arrT = t.toCharArray() ;
-        Arrays.sort(arrS);
-        Arrays.sort(arrT);
-        return Arrays.equals(arrS,arrT) ;
+        HashMap<Character , Integer> map = new HashMap<>();
+        for(char c : s.toCharArray())
+            map.put(c , map.getOrDefault(c , 0) + 1);
+        
+        for(char c : t.toCharArray()){
+            if(!map.containsKey(c)) return false;
+            map.put(c , map.get(c) - 1);
+            if(map.get(c) == 0) map.remove(c);
+        }
+        return map.isEmpty();
     }
 }
