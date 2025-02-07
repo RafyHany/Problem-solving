69323049b8616a0e6cfaf8be7db83b2de14920ed
Rafi Hany Said Ansy Abdel Malak <121082388+RafyHany@users.noreply.github.com>
2025-02-07 12:28:23 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3160-find-the-number-of-distinct-colors-among-the-balls/3160-find-the-number-of-distinct-colors-among-the-balls.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] queryResults(int limit, int[][] queries) {
+        HashMap<Integer , Integer> ballWithColor = new HashMap<>();
+        HashMap<Integer , Integer> colorUsedWithFreq = new HashMap<>();
+        int[] res = new int[queries.length] ;
+        for(int i = 0 ; i < queries.length ; ++i){
+            int ball = queries[i][0] ;
+            int color = queries[i][1] ;
+            if(ballWithColor.containsKey(ball)){
+                int usedColor = ballWithColor.get(ball);
+                int freqOfColor = colorUsedWithFreq.get(usedColor) ;
+                if(freqOfColor == 1)  
+                    colorUsedWithFreq.remove(usedColor);
+                else 
+                    colorUsedWithFreq.put(usedColor , freqOfColor - 1);
+            }else 
+                ballWithColor.put(ball , color);
+            colorUsedWithFreq.put(color , colorUsedWithFreq.getOrDefault(color , 0) + 1);
+            res[i] = colorUsedWithFreq.size();
+        }
+        return res ;
+    }
+}
